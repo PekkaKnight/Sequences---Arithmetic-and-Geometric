9c95be4ab76b5d755ddfa41490e411a34373641b
Urjit Aich <98252856+PekkaKnight@users.noreply.github.com>
2022-02-17 02:55:37 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,11 +2,10 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        int[] arr = {3, 6, 12, 24, 48};
+        int[] arr = {3, 6, 12, 24, 48};   //Array can be whatever you want. Insertion Sort changes into ascending order no matter what
         Sequence test  = new Sequence(arr);
         Internal test2 = new Internal(arr);
-        test.print();
+        test.print();   //Prints out thr array
         if(test.isArithmetic() == true){
             System.out.println("The sequence is " + test.specifySequence() + " and " + "the Common Difference is " + test2.getCommonDifference());
         }
